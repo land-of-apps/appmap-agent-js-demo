3284fbc94e3c6ebebf41710254121dde41914efd
Laurent Christophe <lachrist@vub.ac.be>
2022-01-07 18:19:10 +0100

    feat: initial commit

---
+++ test/index.mjs
@@ -0,0 +1,11 @@
+
+import { main } from "../lib/index.mjs";
+import { strict as Assert } from "assert";
+
+describe('index', () => {
+  describe('main', () => {
+    it('should return main', () => {
+      Assert.equal(main(), "main");
+    });
+  });
+});
