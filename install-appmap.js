a5078bb8e59fc6eb2b8bd03b05ea802f577445ed
Laurent Christophe <lachrist@vub.ac.be>
2022-01-10 11:06:57 +0100

    fix: missing path in installation script

--- install-appmap.js
+++ install-appmap.js
@@ -1,21 +1,21 @@
 const { writeFileSync, readFileSync } = require("fs");
 const { execSync } = require("child_process");
 
-// 1. Install the agent:
+console.log("Installing the agent...");
 execSync("npm install --save-dev '@appland/appmap-agent-js'", {
   stdio: "inherit",
 });
-// 2. Add configuration:
+
+console.log("Adding configuration file...");
 writeFileSync("appmap.yml", "packages:\n  - glob: src/*", "utf8");
-// 3. Add some npm scripts:
-{
-  const npm = JSON.parse(readFileSync("package.json", "utf8"));
-  const { test, start } = npm.scripts;
-  npm.scripts = {
-    ...npm.scripts,
-    "appmap-test": `npx appmap-agent-js --recorder=mocha -- ${test}`,
-    "appmap-start": `npx appmap-agent-js --recorder=process -- ${start}`,
-    "appmap-start-partial": `npx appmap-agent-js --recorder=remote -- ${start}`,
-  };
-  writeFileSync(JSON.stringify(npm, null, 2), "utf8");
-}
+
+console.log("Adding some npm scripts...");
+const npm = JSON.parse(readFileSync("package.json", "utf8"));
+const { test, start } = npm.scripts;
+npm.scripts = {
+  ...npm.scripts,
+  "appmap-test": `npx appmap-agent-js --recorder=mocha -- ${test}`,
+  "appmap-start": `npx appmap-agent-js --recorder=process -- ${start}`,
+  "appmap-start-partial": `npx appmap-agent-js --recorder=remote -- ${start}`,
+};
+writeFileSync("package.json", JSON.stringify(npm, null, 2), "utf8");
