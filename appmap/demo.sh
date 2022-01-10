b81df57e99e2f677275b9dfde10e6c7102d8ac33
Laurent Christophe <lachrist@vub.ac.be>
2022-01-10 14:28:57 +0100

    doc: demo recording

--- appmap/demo.sh
+++ appmap/demo.sh
@@ -1,4 +1,4 @@
-npm install --save-dev @appland/appmap-agent-js
+npm install --silent --save-dev @appland/appmap-agent-js
 # vim appmap.yml
 # log: error
 # packages:
@@ -6,7 +6,7 @@ npm install --save-dev @appland/appmap-agent-js
 echo 'log: error\npackages:\n  glob: src/*' > appmap.yml
 
 npx appmap-agent-js --recorder process -- node bin/bin.js 8080 db.sqlite3
-cat tmp/appmap/anonymous.appmap.json
+vim tmp/appmap/anonymous.appmap.json
 
 npx appmap-agent-js --recorder mocha -- npx mocha -r ts-node/register 'test/*.ts'
 ls tmp/appmap/mocha
