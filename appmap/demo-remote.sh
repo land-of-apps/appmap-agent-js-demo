611ad5dc714eed7e9530f1062e8f97134d4930a6
Laurent Christophe <lachrist@vub.ac.be>
2022-05-31 10:05:22 +0200

    doc: add remote recording demo

--- appmap/demo-remote.sh
+++ appmap/demo-remote.sh
@@ -1,3 +1,8 @@
-npx appmap-agent-js --recorder=remote -- node bin/bin.js 8080 db.sqlite3
+ls
+cat appmap.yml
+npx appmap-agent-js --recorder=remote --track-port=3000 -- node bin/bin.js 8080 db.sqlite3 &
+curl -X POST http://localhost:3000/_appmap/record
 curl -X POST http://localhost:8080/signup/lachrist@appland/1234/laurent
-curl http://localhost:8080/signin/lachrist@appland/1234
+curl -X GET http://localhost:8080/signin/lachrist@appland/1234
+curl -X DELETE http://localhost:3000/_appmap/record
+kill -s SIGINT $!
